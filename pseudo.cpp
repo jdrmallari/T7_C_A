a37ec5be6b557182f7d689b02ea95f60759de774
Lachlan <lachlan.rowley@students.mq.edu.au>
2021-09-08 13:37:38 +1000

    updated pseudo code

--- pseudo.cpp
+++ pseudo.cpp
@@ -1,42 +1,118 @@
-void function setup() {
-    pinMode(wheel_L_FORWARD, OUTPUT);
-    pinMode(wheel_L_BACK, OUTPUT);
-    pinMode(wheel_R_FORWARD, OUTPUT);
-    pinMode(wheel_R_BACK, OUTPUT);
-    Serial.begin(9600);
+const int WHEEL_L_FORWARD = 0;
+const int WHEEL_L_BACK = 0;
+const int WHEEL_R_FORWARD = 0;
+const int WHEEL_R_BACK = 0;
+const int HIGH = 1;
+const int LOW = 0;
+const int OUTPUT = 0;
+const int PIN1 = 0;
+const int PIN2 = 0;
+const int OPEN_DEGREE = 0;
+const int CLOSE_DEGREE = 0;
+
+class Serial {
+    public:
+        void begin(int a) {};
+        char read(){return 'a';};
+};
+
+class Servo {
+    public:
+        void write(int a) {};
+};
+
+class Encoder {
+    public:
+        Encoder(int a, int b) {};
+        int read() {return 1;};
+};
+
+Serial serial;
+Servo servo;
+
+void delay(int a) {};
+
+void digitalWrite(int a, int b) {
+
 }
 
-void function readInput() {
-	input = Serial.Read();
-	
-	switch(input) {
-		case(F):
-			moveForward();
-		case(B):
-			moveBackward();
-		case(L):
-			moveLeft();
-		case(R):
-			moveRight();
-		case(START):
-			start();
-		case(EXIT):
-			exit();		
-	}
+void pinMode(int a, int b) {
+}
+
+void moveLeft(){};
+void moveRight(){};
+void start(){};
+void stop(){};
+
+void setup() {
+    pinMode(WHEEL_L_FORWARD, OUTPUT);
+    pinMode(WHEEL_L_BACK, OUTPUT);
+    pinMode(WHEEL_R_FORWARD, OUTPUT);
+    pinMode(WHEEL_R_BACK, OUTPUT);
+    serial.begin(9600);
+}
+
+void moveForward() {
+    digitalWrite(WHEEL_L_FORWARD, HIGH);
+    digitalWrite(WHEEL_L_BACK, LOW);
+    digitalWrite(WHEEL_R_FORWARD, HIGH);
+    digitalWrite(WHEEL_R_BACK, LOW);
+}
+
+void moveBackward() {
+    digitalWrite(WHEEL_L_FORWARD, LOW);
+    digitalWrite(WHEEL_L_BACK, HIGH);
+    digitalWrite(WHEEL_R_FORWARD, LOW);
+    digitalWrite(WHEEL_R_BACK, HIGH);
+}
+
+void readInput() {
+    char input = serial.read();
+    switch(input) {
+        case('a'):
+            moveForward();
+            break;
+        case('b'):
+            moveBackward();
+            break;
+        case('c'):
+            moveLeft();
+            break;
+        case('d'):
+            moveRight();
+            break;
+        case('e'):
+            start();
+            break;
+        case('f'):
+            stop();
+            break;
+        case('g'):
+            disposal(true);
+            break;
+        case('h'):
+            disposal(false);
+            break;
+    }
 }
 
 
-void function moveForward() {
-	digitalWrite( wheel_L_FORWARD, HIGH);
-   	digitalWrite( wheel_L_BACK, LOW);
-   	digitalWrite( wheel_R_FORWARD, HIGH);
-   	digitalWrite( wheel_R_BACK, LOW);
+Encoder rotarycs(PIN1, PIN2);
+int rotaryPos = 0; // Initial encoder position
+
+void loop() {
+    int newPos;
+    newPos = rotarycs.read();
+    if (newPos != rotaryPos)
+        rotaryPos = newPos;
 }
 
-void function moveBackward() {
-	digitalWrite( wheel_L_FORWARD, LOW);
-   	digitalWrite( wheel_L_BACK, HIGH);
-   	digitalWrite( wheel_R_FORWARD, LOW);
-   	digitalWrite( wheel_R_BACK, HIGH);
+void disposal(bool open) {
+    if(open) {
+        servo.write(OPEN_DEGREE); // Turn the servo to its proper angle
+    }
+    else {
+        servo.write(CLOSE_DEGREE);
+    }
+    delay(100); //	Delay a period of time for servo to rotate to the corresponding position
 }
-
